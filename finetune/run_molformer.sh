9639b4ea8e0452b99af544080fa0c69e5fe1b328
Han Ni Lin <hl4138@gadi-login-05.gadi.nci.org.au>
2025-02-06 23:39:29 +1100

    update files

---
+++ finetune/run_molformer.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#PBS -q gpuvolta
+#PBS -P um09
+#PBS -l ncpus=48
+#PBS -l ngpus=4
+#PBS -l mem=128GB
+#PBS -l walltime=35:00:00
+#PBS -l storage=scratch/um09
+#PBS -l jobfs=100GB
+
+cd /scratch/um09/hl4138
+module load python3/3.8.5 cuda/11.0.3
+module list
+
+source molformer-venv/bin/activate
+cd molformer/finetune
+
+./run_finetune_u0.sh
